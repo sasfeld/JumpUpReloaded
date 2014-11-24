ad2c2b9f1cb41680db117c34494862a6e66019e5
Sascha Feldmann <sascha.feldmann@gmx.de>
2014-11-24 19:04:06 +0100

    #351560 improved registration

---
+++ src/main/java/de/htw/fb4/imi/jumpup/util/ErrorPrintable.java
@@ -0,0 +1,38 @@
+/**
+ * JumpUp.Me Car Pooling Application
+ *
+ * Copyright (c) 2014 Sebastian Renner, Marco Seidler, Sascha Feldmann
+ */
+package de.htw.fb4.imi.jumpup.util;
+
+/**
+ * <p>Error printable models can define error methods that need to be presented to the user in the frontend.</p>
+ *
+ * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
+ * @since 24.11.2014
+ *
+ */
+public interface ErrorPrintable
+{
+
+    /**
+     * Check whether an error was raised.
+     * @return
+     */
+    boolean hasError();
+    
+    /**
+     * Return all error messages.
+     * 
+     * Don't translate the error messages. This should be done by the client.
+     * 
+     * @return
+     */
+    String[] getErrors();
+    
+    /**
+     * Get a single error string using the given delimiter (such as "\n").
+     * @return
+     */
+    String getSingleErrorString();
+}
