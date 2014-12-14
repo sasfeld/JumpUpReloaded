f214b671a183e1087f844013efff42ba0001e4a8
GammaG <m_seidler@hotmail.de>
2014-12-14 16:15:00 +0100

    Changes for Debug logging

--- src/main/java/de/htw/fb4/imi/jumpup/Application.java
+++ src/main/java/de/htw/fb4/imi/jumpup/Application.java
@@ -8,6 +8,7 @@ package de.htw.fb4.imi.jumpup;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -16,71 +17,118 @@ import org.jboss.logging.Logger;
 import de.htw.fb4.imi.jumpup.settings.PersistenceSettings;
 
 /**
- * <p>The central static facade of our application.</p>
- *
+ * <p>
+ * The central static facade of our application.
+ * </p>
+ * 
  * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
  * @since 24.11.2014
- *
+ * 
  */
 public class Application
 {
     @PersistenceContext(name = PersistenceSettings.PERSISTENCE_UNIT)
     protected EntityManager entityManager;
-    
+
+    protected static final boolean debugMode;
+
+    static {
+        jBossLoggers = new HashMap<>();
+        String s = FacesContext.getCurrentInstance().getExternalContext()
+                .getInitParameter("javax.faces.PROJECT_STAGE");
+
+        log("JSF Config Mode: " + s, LogType.INFO, Application.class);
+
+        debugMode = s.equals("Development") ? true : false;
+    }
+
     /**
      * 
-     * <p>The LogType enumeration follows the strategy by implementing a log() method for each strategy.</p>
-     *
+     * <p>
+     * The LogType enumeration follows the strategy by implementing a log()
+     * method for each strategy.
+     * </p>
+     * 
      * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
      * @since 24.11.2014
-     *
+     * 
      */
-    public enum LogType {
-        INFO() 
+    public enum LogType
+    {
+        INFO()
         {
             @Override
             /*
              * (non-Javadoc)
-             * @see de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String, org.jboss.logging.Logger)
+             * 
+             * @see
+             * de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String,
+             * org.jboss.logging.Logger)
              */
             public <T> void log(final String message, final Logger jBossLogger)
             {
                 jBossLogger.info(message);
             }
-        }, 
-        
+        },
+
+        DEBUG()
+        {
+            @Override
+            /*
+             * (non-Javadoc)
+             * 
+             * @see
+             * de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String,
+             * org.jboss.logging.Logger)
+             */
+            public <T> void log(final String message, final Logger jBossLogger)
+            {
+                if (debugMode)
+                    jBossLogger.info(message);
+            }
+        },
+
         WARNING
         {
             @Override
             /*
              * (non-Javadoc)
-             * @see de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String, org.jboss.logging.Logger)
+             * 
+             * @see
+             * de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String,
+             * org.jboss.logging.Logger)
              */
             public <T> void log(final String message, final Logger jBossLogger)
             {
                 jBossLogger.debug(message);
             }
-        }, 
-        
-        ERROR 
+        },
+
+        ERROR
         {
             @Override
             /*
              * (non-Javadoc)
-             * @see de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String, org.jboss.logging.Logger)
+             * 
+             * @see
+             * de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String,
+             * org.jboss.logging.Logger)
              */
             public <T> void log(final String message, final Logger jBossLogger)
             {
                 jBossLogger.error(message);
             }
-        }, 
-        
+        },
+
         CRITICAL
         {
             @Override
             /*
              * (non-Javadoc)
-             * @see de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String, org.jboss.logging.Logger)
+             * 
+             * @see
+             * de.htw.fb4.imi.jumpup.Application.LogType#log(java.lang.String,
+             * org.jboss.logging.Logger)
              */
             public <T> void log(final String message, final Logger jBossLogger)
             {
@@ -90,22 +138,25 @@ public class Application
 
         /**
          * Log the given message using the given JBoss Logger.
+         * 
          * @param message
          * @param jBossLogger
          * @throws UnsupportedOperationException
          */
         public <T> void log(final String message, final Logger jBossLogger)
         {
-            throw new UnsupportedOperationException("The enumeration value " + this + " doesn't implement the method log(). Please implement it!");
+            throw new UnsupportedOperationException(
+                    "The enumeration value "
+                            + this
+                            + " doesn't implement the method log(). Please implement it!");
         }
     }
-    
+
     /**
      * JBoss Loggers attached to the Java classes.
      */
-    protected static Map<Class<?>, Logger> jBossLoggers = new HashMap<>();  
-    
-   
+    protected static Map<Class<?>, Logger> jBossLoggers;
+
     /**
      * Central facade method for using our logging system / adaption.
      * 
@@ -113,16 +164,18 @@ public class Application
      * @param logType
      * @param clazz
      */
-    public static <T> void log(final String message, final LogType logType, final Class<T> clazz) 
+    public static <T> void log(final String message, final LogType logType,
+            final Class<T> clazz)
     {
         addLoggerIfNotYetDone(clazz);
-        
+
         // Log using JBoss
         logType.log(message, jBossLoggers.get(clazz));
     }
-    
+
     /**
      * Add logger being lazy.
+     * 
      * @param clazz
      */
     protected static <T> void addLoggerIfNotYetDone(final Class<T> clazz)
@@ -130,5 +183,5 @@ public class Application
         if (!jBossLoggers.containsKey(clazz)) {
             jBossLoggers.put(clazz, Logger.getLogger(clazz));
         }
-    }    
+    }
 }
--- src/main/java/de/htw/fb4/imi/jumpup/user/controllers/Login.java
+++ src/main/java/de/htw/fb4/imi/jumpup/user/controllers/Login.java
@@ -11,6 +11,8 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import de.htw.fb4.imi.jumpup.Application;
+import de.htw.fb4.imi.jumpup.Application.LogType;
 import de.htw.fb4.imi.jumpup.controllers.AbstractFacesController;
 import de.htw.fb4.imi.jumpup.navigation.NavigationOutcomes;
 import de.htw.fb4.imi.jumpup.settings.BeanNames;
@@ -18,15 +20,21 @@ import de.htw.fb4.imi.jumpup.user.login.LoginMethod;
 import de.htw.fb4.imi.jumpup.user.login.LoginModel;
 
 /**
- * <p>The Login controller is used for the login action and to check if the user is authenticated.</p>
+ * <p>
+ * The Login controller is used for the login action and to check if the user is
+ * authenticated.
+ * </p>
  * 
- * <p>It is sessionscoped, so that you can access it from everywhere to get the current logged in user.</p>
- *
+ * <p>
+ * It is sessionscoped, so that you can access it from everywhere to get the
+ * current logged in user.
+ * </p>
+ * 
  * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
  * @since 27.11.2014
- *
+ * 
  */
-@Named( value = BeanNames.LOGIN_CONTROLLER )
+@Named(value = BeanNames.LOGIN_CONTROLLER)
 @SessionScoped
 public class Login extends AbstractFacesController implements Serializable
 {
@@ -36,7 +44,7 @@ public class Login extends AbstractFacesController implements Serializable
     private static final long serialVersionUID = 6762395393648784704L;
 
     protected LoginModel loginModel = new LoginModel();
-    
+
     @Inject
     protected LoginMethod loginMethod;
 
@@ -45,10 +53,10 @@ public class Login extends AbstractFacesController implements Serializable
      */
     public LoginModel getLoginModel()
     {
+
         return loginModel;
     }
-    
-    
+
     /**
      * LogIn user action.
      * 
@@ -56,53 +64,59 @@ public class Login extends AbstractFacesController implements Serializable
      */
     public String loginUser()
     {
-        try {       
-            this.loginMethod.logIn(this.getLoginModel());  
-            
-            // registration was performed successfully, so redirect to success page
+        try {
+            this.loginMethod.logIn(this.getLoginModel());
+
+            Application.log("Login: User tried to login.", LogType.INFO,
+                    getClass());
+            // registration was performed successfully, so redirect to success
+            // page
             if (!this.loginMethod.hasError()) {
                 return NavigationOutcomes.LOGIN_SUCCESS;
             }
 
             // otherwise add all error messages
-            for (final String errorMessage: this.loginMethod.getErrors()) {
+            for (final String errorMessage : this.loginMethod.getErrors()) {
                 this.addDisplayErrorMessage(errorMessage);
-            }            
-        } catch ( Exception e ) {
-            this.addDisplayErrorMessage("I was not able to log you in.");            
+            }
+        } catch (Exception e) {
+            this.addDisplayErrorMessage("I was not able to log you in.");
         }
-        
+
         return NavigationOutcomes.LOGIN_FAILURE;
     }
-    
+
     /**
      * Log-out user action.
+     * 
      * @return
      */
     public String logoutUser()
     {
-        try {       
-            this.loginMethod.logOut(this.getLoginModel());  
-            
-            // registration was performed successfully, so redirect to success page
+        try {
+            this.loginMethod.logOut(this.getLoginModel());
+
+            // registration was performed successfully, so redirect to success
+            // page
             if (!this.loginMethod.hasError()) {
                 this.addDisplayInfoMessage("You were successfully logged out. We hope you enjoyed your stay.");
                 return NavigationOutcomes.LOGOUT_SUCCESS;
             }
 
             // otherwise add all error messages
-            for (final String errorMessage: this.loginMethod.getErrors()) {
+            for (final String errorMessage : this.loginMethod.getErrors()) {
                 this.addDisplayErrorMessage(errorMessage);
-            }            
-        } catch ( Exception e ) {
-            this.addDisplayErrorMessage("I was not able to log you out.");            
+            }
+        } catch (Exception e) {
+            this.addDisplayErrorMessage("I was not able to log you out.");
         }
-        
+
         return NavigationOutcomes.LOGOUT_FAILURE;
     }
 
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#toString()
      */
     @Override
@@ -116,6 +130,5 @@ public class Login extends AbstractFacesController implements Serializable
         builder.append("]");
         return builder.toString();
     }
-    
-    
+
 }
