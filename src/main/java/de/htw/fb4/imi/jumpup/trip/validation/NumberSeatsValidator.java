6a6c21b2d6077f2633e4fa6f847d3578280eb293
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-01-18 18:02:48 +0100

    #357100 added all other components

---
+++ src/main/java/de/htw/fb4/imi/jumpup/trip/validation/NumberSeatsValidator.java
@@ -0,0 +1,107 @@
+/**
+ * JumpUp.Me Car Pooling Application
+ *
+ * Copyright (c) 2014-2015 Sebastian Renner, Marco Seidler, Sascha Feldmann
+ */
+package de.htw.fb4.imi.jumpup.trip.validation;
+
+import javax.inject.Named;
+
+import de.htw.fb4.imi.jumpup.settings.BeanNames;
+import de.htw.fb4.imi.jumpup.trip.entities.Trip;
+import de.htw.fb4.imi.jumpup.trip.util.TripConfigKeys;
+
+/**
+ * <p>Validator for prices configured on {@link Trip}</p>
+ *
+ * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
+ * @since 18.01.2015
+ *
+ */
+@Named(value = BeanNames.NUMBER_SEATS_VALIDATOR)
+public class NumberSeatsValidator extends AbstractTripValidator
+{
+    @Override
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.util.JumpUpValidator#validate(java.lang.Object)
+     */
+    public boolean validate(final Object value)
+    {
+        final Integer numberSeats = castToInt(value);
+        
+        if (null == numberSeats || !this.checkRange(numberSeats)) {
+                return false;
+            }
+        
+        return true;
+    }
+    
+    private Integer castToInt(Object value)
+    {
+        if (value instanceof String) {
+            try {
+                return Integer.parseInt((String) value);
+            } catch (NumberFormatException e) {
+                errorMessages.add("Please type in valid number of seats as decimal.");
+                return null;
+            }
+        } else if (value instanceof Integer) {
+            return (Integer) value;
+        } else {
+            errorMessages.add("Please type in a valid number of seats as decimal.");
+            return null;
+        }
+    }
+
+    private boolean checkRange(Integer numberSeats)
+    {
+        Float minNumberSeats = getMinNumberOfSeats(); 
+        Float maxNumberSeats = getMaxNumberOfSeats(); 
+         
+        if (numberSeats >= minNumberSeats && numberSeats <= maxNumberSeats) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public Float getMaxNumberOfSeats()
+    {
+        Float maxPrice = Float.parseFloat(this.tripConfigReader.fetchValue(TripConfigKeys.JUMPUP_TRIP_VALIDATION_NUMBER_SEATS_MAX_VALUE));
+        return maxPrice;
+    }
+
+    public Float getMinNumberOfSeats()
+    {
+        Float minPrice = Float.parseFloat(this.tripConfigReader.fetchValue(TripConfigKeys.JUMPUP_TRIP_VALIDATION_NUMBER_SEATS_MIN_VALUE));
+        return minPrice;
+    }
+
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.JumpUpValidator#getMinLength()
+     */
+    @Override
+    public int getMinLength()
+    {
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.JumpUpValidator#getMaxLength()
+     */
+    @Override
+    public int getMaxLength()
+    {
+        return 0;
+    }
+
+    @Override
+    /*
+     * (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.AbstractValidator#getDefaultFailureMessage()
+     */
+    protected String getDefaultFailureMessage()
+    {
+        return "Please enter number of seats between " + this.getMinNumberOfSeats() + " and " + this.getMaxNumberOfSeats() + " euros.";
+    }
+}
---
+++ src/main/java/de/htw/fb4/imi/jumpup/trip/validation/OverviewPath.java
@@ -0,0 +1,61 @@
+/**
+ * JumpUp.Me Car Pooling Application
+ *
+ * Copyright (c) 2014-2015 Sebastian Renner, Marco Seidler, Sascha Feldmann
+ */
+package de.htw.fb4.imi.jumpup.trip.validation;
+
+import javax.inject.Named;
+
+import de.htw.fb4.imi.jumpup.settings.BeanNames;
+
+/**
+ * <p>Validator for overview path, a concatenated string of LatLng values that mark the driver's route.</p>
+ *
+ * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
+ * @since 18.01.2015
+ *
+ */
+@Named(value = BeanNames.OVERVIEW_PATH_VALIDATOR)
+public class OverviewPath extends AbstractTripValidator
+{
+
+    @Override
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.util.JumpUpValidator#validate(java.lang.Object)
+     */
+    public boolean validate(final Object value)
+    {        
+        // TODO implement validation
+        
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.JumpUpValidator#getMinLength()
+     */
+    @Override
+    public int getMinLength()
+    {
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.JumpUpValidator#getMaxLength()
+     */
+    @Override
+    public int getMaxLength()
+    {
+        return 0;
+    }
+
+    @Override
+    /*
+     * (non-Javadoc)
+     * @see de.htw.fb4.imi.jumpup.validator.AbstractValidator#getDefaultFailureMessage()
+     */
+    protected String getDefaultFailureMessage()
+    {
+        return "Invalid overview path given. Did you plan your trip on the map?";
+    }
+}
