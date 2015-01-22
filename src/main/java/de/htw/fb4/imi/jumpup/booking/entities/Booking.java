735707312e212ef45f83935f24743c2cdc53c899
GammaG <m_seidler@hotmail.de>
2015-01-22 17:23:58 +0100

    #354222 Booking now has getter/setter toString equals and hash

--- src/main/java/de/htw/fb4/imi/jumpup/booking/entities/Booking.java
+++ src/main/java/de/htw/fb4/imi/jumpup/booking/entities/Booking.java
@@ -70,4 +70,163 @@ public class Booking extends AbstractEntity
     @JoinColumn(name = "bookings")
     private User passenger;
 
+    public String getStartPoint()
+    {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint)
+    {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint()
+    {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint)
+    {
+        this.endPoint = endPoint;
+    }
+
+    public double getStartLatitude()
+    {
+        return startLatitude;
+    }
+
+    public void setStartLatitude(double startLatitude)
+    {
+        this.startLatitude = startLatitude;
+    }
+
+    public double getStartLongitude()
+    {
+        return startLongitude;
+    }
+
+    public void setStartLongitude(double startLongitude)
+    {
+        this.startLongitude = startLongitude;
+    }
+
+    public double getEndLatitude()
+    {
+        return endLatitude;
+    }
+
+    public void setEndLatitude(double endLatitude)
+    {
+        this.endLatitude = endLatitude;
+    }
+
+    public double getEndLongitude()
+    {
+        return endLongitude;
+    }
+
+    public void setEndLongitude(double endLongitude)
+    {
+        this.endLongitude = endLongitude;
+    }
+
+    public Trip getTrip()
+    {
+        return trip;
+    }
+
+    public void setTrip(Trip trip)
+    {
+        this.trip = trip;
+    }
+
+    public User getPassenger()
+    {
+        return passenger;
+    }
+
+    public void setPassenger(User passenger)
+    {
+        this.passenger = passenger;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Booking [startPoint=" + startPoint + ", endPoint=" + endPoint
+                + ", startLatitude=" + startLatitude + ", startLongitude="
+                + startLongitude + ", endLatitude=" + endLatitude
+                + ", endLongitude=" + endLongitude + ", trip=" + trip
+                + ", passenger=" + passenger + "]";
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(endLatitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(endLongitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result
+                + ((endPoint == null) ? 0 : endPoint.hashCode());
+        result = prime * result
+                + ((passenger == null) ? 0 : passenger.hashCode());
+        temp = Double.doubleToLongBits(startLatitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(startLongitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result
+                + ((startPoint == null) ? 0 : startPoint.hashCode());
+        result = prime * result + ((trip == null) ? 0 : trip.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Booking other = (Booking) obj;
+        if (Double.doubleToLongBits(endLatitude) != Double
+                .doubleToLongBits(other.endLatitude))
+            return false;
+        if (Double.doubleToLongBits(endLongitude) != Double
+                .doubleToLongBits(other.endLongitude))
+            return false;
+        if (endPoint == null) {
+            if (other.endPoint != null)
+                return false;
+        } else if (!endPoint.equals(other.endPoint))
+            return false;
+        if (passenger == null) {
+            if (other.passenger != null)
+                return false;
+        } else if (!passenger.equals(other.passenger))
+            return false;
+        if (Double.doubleToLongBits(startLatitude) != Double
+                .doubleToLongBits(other.startLatitude))
+            return false;
+        if (Double.doubleToLongBits(startLongitude) != Double
+                .doubleToLongBits(other.startLongitude))
+            return false;
+        if (startPoint == null) {
+            if (other.startPoint != null)
+                return false;
+        } else if (!startPoint.equals(other.startPoint))
+            return false;
+        if (trip == null) {
+            if (other.trip != null)
+                return false;
+        } else if (!trip.equals(other.trip))
+            return false;
+        return true;
+    }
+
 }
