26830bf1d4485257818d01e56eb13a538794ddd8
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-01-24 19:19:53 +0100

    #357105 implemented further testdata in NearbyTripsFilterTest which proved that our main business algorithm is working correct, nice :)

--- src/test/java/de/htw/fb4/imi/jumpup/trip/query/filter/NearbyTripsFilterTest.java
+++ src/test/java/de/htw/fb4/imi/jumpup/trip/query/filter/NearbyTripsFilterTest.java
@@ -22,6 +22,8 @@ import de.htw.fb4.imi.jumpup.trip.query.TripSearchCriteria;
 
 /**
  * <p>Test of {@link NearbyTripsFilter} to find passenger's trips that are near the driver's route.</p>
+ * 
+ * <p>This test works parameterized by a test dataset which consists of expected {@link Trip} lists, {@link TripSearchCriteria} simulating a passenger's trip search and {@link Trip} entities that could have been loaded from a database.</p>
  *
  * @author <a href="mailto:me@saschafeldmann.de">Sascha Feldmann</a>
  * @since 24.01.2015
@@ -59,12 +61,24 @@ public class NearbyTripsFilterTest
                 resultingTrips.toArray(new Trip[resultingTrips.size()]) );
     }
     
+    
+    /*
+     * ############ TEST DATA ###############
+     */   
+    
     @Parameters
     public static Iterable<Object[]> data() 
     {
         return Arrays.asList(
                 new Object[][] {
-                        {getOriginalTripList1(), getTripSearchCriteria1(), getExpectedTripList1()}     
+                        // test 1: given several trips including one from Berlin -> Leipzig and a search query for Berlin -> Leipzig, than expect Berlin -> Leipzig as only element in trip list
+                        {getOriginalTripList1(), getTripSearchCriteria1(), getExpectedTripList1()},  
+                        // test 2: given several trips including one from Berlin -> Trier and a search query for Leipzig -> Erfurt, than expect Leipzig -> Erfurt and Berlin -> Trier as results
+                        {getOriginalTripList2(), getTripSearchCriteria2(), getExpectedTripList2()},  
+                        // test 3: given several trips and search from Weimar -> Trier, expect Berlin -> Trier and Weimar -> Trier to be in result list
+                        {getOriginalTripList3(), getTripSearchCriteria3(), getExpectedTripList3()},  
+                        // test 4: basically same as test 2, but maxDistance is set to 0, then Leipzig -> Erfurt should be the only resulting route
+                        {getOriginalTripList4(), getTripSearchCriteria4(), getExpectedTripList4()}, 
                 });
     }
 
@@ -90,6 +104,89 @@ public class NearbyTripsFilterTest
         tripList.add(FilterTestHelper.getTripFromBerlinToLeipzig());
         tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
         tripList.add(FilterTestHelper.getTripFromWeimarToTrier());
+        
+        return tripList;
+    }
+    
+    private static List<Trip> getExpectedTripList2()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
+        tripList.add(FilterTestHelper.getTripFromBerlinToTrier());
+        
+        return tripList;
+    }
+    
+    private static TripSearchCriteria getTripSearchCriteria2()
+    {
+        // max distance to trips = 10km
+        return FilterTestHelper.getTripSearchFromLeipzigToErfurt(20);
+    }
+    
+    private static List<Trip>  getOriginalTripList2()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromBerlinToLeipzig());
+        tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
+        tripList.add(FilterTestHelper.getTripFromWeimarToTrier());
+        tripList.add(FilterTestHelper.getTripFromBerlinToTrier());
+        
+        return tripList;
+    }
+    
+    private static List<Trip> getExpectedTripList3()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromWeimarToTrier());
+        tripList.add(FilterTestHelper.getTripFromBerlinToTrier());
+        
+        return tripList;
+    }
+    
+    private static TripSearchCriteria getTripSearchCriteria3()
+    {
+        // max distance to trips = 10km
+        return FilterTestHelper.getTripSearchFromWeimarToTrier(20);
+    }
+    
+    private static List<Trip>  getOriginalTripList3()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromBerlinToLeipzig());
+        tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
+        tripList.add(FilterTestHelper.getTripFromWeimarToTrier());
+        tripList.add(FilterTestHelper.getTripFromBerlinToTrier());
+        
+        return tripList;
+    }
+    
+    private static List<Trip> getExpectedTripList4()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
+        
+        return tripList;
+    }
+    
+    private static TripSearchCriteria getTripSearchCriteria4()
+    {
+        // max distance to trips = 10km
+        return FilterTestHelper.getTripSearchFromLeipzigToErfurt(1);
+    }
+    
+    private static List<Trip>  getOriginalTripList4()
+    {
+        List<Trip> tripList = new ArrayList<Trip>();
+        
+        tripList.add(FilterTestHelper.getTripFromBerlinToLeipzig());
+        tripList.add(FilterTestHelper.getTripFromLeipzigToErfurt());
+        tripList.add(FilterTestHelper.getTripFromWeimarToTrier());
+        tripList.add(FilterTestHelper.getTripFromBerlinToTrier());
         
         return tripList;
     }
